78755c49e7a88dd4d23675b9f7f2fcd97926f7be
Tomasnuin <tomasnuingas@gmail.com>
2023-03-11 11:33:46 -0300

    Modelo Imagen creado, renombrado MiException a MiExcepcion, Rol movido a Enumeraciones

---
+++ src/main/java/com/egg/alquileres/entidades/Imagen.java
@@ -0,0 +1,55 @@
+package com.egg.alquileres.entidades;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Imagen implements Serializable {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String idImagen;
+
+    private String nombre;
+    private String formato;
+    private byte[] contenido;
+
+    public Imagen() {
+    }
+
+    public String getIdImagen() {
+        return idImagen;
+    }
+
+    public void setIdImagen(String idImagen) {
+        this.idImagen = idImagen;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFormato() {
+        return formato;
+    }
+
+    public void setFormato(String formato) {
+        this.formato = formato;
+    }
+
+    public byte[] getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(byte[] contenido) {
+        this.contenido = contenido;
+    }
+}
