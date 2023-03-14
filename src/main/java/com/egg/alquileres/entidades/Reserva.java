44bd823ee998cdd05c21df05577352dbf8c54cee
Tomasnuin <tomasnuingas@gmail.com>
2023-03-14 08:49:57 -0300

    Entidades FechasDisponibles, Prestacion y reserva modificadas

--- src/main/java/com/egg/alquileres/entidades/Reserva.java
+++ src/main/java/com/egg/alquileres/entidades/Reserva.java
@@ -1,55 +1,3 @@
-<<<<<<< Updated upstream
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.egg.alquileres.entidades;
-
-import java.util.Date;
-import java.util.List;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Temporal;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.hibernate.annotations.GenericGenerator;
-
-/**
- *
- * @author Luz
- */
-@Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-public class Reserva {
-
-    @Id
-    @GeneratedValue(generator = "uuid")
-    @GenericGenerator(name = "uuid", strategy = "uuid2")
-    private String idReserva;
-
-    @Temporal(javax.persistence.TemporalType.DATE)
-    private Date fechaDesde;
-    @Temporal(javax.persistence.TemporalType.DATE)
-    private Date fechaHasta;
-
-    private Double precio;
-    
-    @OneToOne
-    private Cliente cliente;
-    @OneToOne
-    private Propiedad propiedad;
-
-}
-=======
 package com.egg.alquileres.entidades;
 
 import java.io.Serializable;
@@ -133,4 +81,3 @@ public class Reserva implements Serializable {
         this.propiedad = propiedad;
     }
 }
->>>>>>> Stashed changes
