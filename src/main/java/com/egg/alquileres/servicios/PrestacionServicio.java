a9f55f512dce6501ef2ed5eefb2341a9b80c7785
facundomedina96 <facumedina.1973@gmail.com>
2023-03-20 21:50:57 -0300

    Modificacion en el servicio de prestacion

--- src/main/java/com/egg/alquileres/servicios/PrestacionServicio.java
+++ src/main/java/com/egg/alquileres/servicios/PrestacionServicio.java
@@ -1,8 +1,12 @@
 package com.egg.alquileres.servicios;
 
+import com.egg.alquileres.entidades.Imagen;
 import com.egg.alquileres.entidades.Prestacion;
+import com.egg.alquileres.entidades.Propiedad;
 import com.egg.alquileres.enumeraciones.NombrePrestacion;
 import com.egg.alquileres.repositorios.PrestacionRepositorio;
+import java.util.Optional;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,5 +24,19 @@ public class PrestacionServicio {
         prestacionRepo.save(prestacion);
         
         return prestacion;
+    }
+    //vista del propietario para que pueda modificar la prestacion de su propiedad
+    public void modificarPrestacion(String id, NombrePrestacion nombre, Double precio){
+        Optional<Prestacion> respuesta = prestacionRepo.findById(id);
+        
+        if(respuesta.isPresent()){
+            Prestacion prestacion = respuesta.get();
+            prestacion.setNombre(nombre);
+            prestacion.setPrecio(precio);
+
+
+            prestacionRepo.save(prestacion);
+        }
+        
     }
 }
