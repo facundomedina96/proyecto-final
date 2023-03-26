7a9d736c1dd864bf475892ecf3a7a8fe7a1942b9
Sofia Raia <sofiaraia246@outlook.es>
2023-03-26 19:31:50 -0300

    cambios

--- src/main/java/com/egg/alquileres/controladores/ReservaControlador.java
+++ src/main/java/com/egg/alquileres/controladores/ReservaControlador.java
@@ -4,12 +4,16 @@
  */
 package com.egg.alquileres.controladores;
 
+import com.egg.alquileres.entidades.Propiedad;
 import com.egg.alquileres.servicios.PropiedadServicio;
 import com.egg.alquileres.servicios.ReservaServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 /**
  *
@@ -22,8 +26,10 @@ public class ReservaControlador {
     @Autowired
     ReservaServicio reservaServicio;
     @PreAuthorize("hasAnyRole('ROLE_CLIENTE')")
-    @GetMapping("/reserva/{id_propiedad}")
-    public String mostrar_reserva_propiedad() {
+    @GetMapping("/reserva/{propiedad_id}")
+    public String mostrar_reserva_propiedad(@PathVariable String propiedad_id, ModelMap model) {
+        Propiedad propiedad = propiedadServicio.getOne(propiedad_id);
+        model.put("propiedad", propiedad);
         return "reserva.html";
     }
 }
